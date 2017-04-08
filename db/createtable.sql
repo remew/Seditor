b3fc7944bebdc0d136395ca0d22d4149ccd0ca12
remew <foolishremew@gmail.com>
2017-04-08 23:17:55 +0900

    add tag table, add tagging table, change table definition

--- db/createtable.sql
+++ db/createtable.sql
@@ -2,24 +2,44 @@
 -- drop to exist tables;
 DROP TABLE IF EXISTS scenario;
 DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS tag;
+
+DROP TABLE IF EXISTS tagging;
 
 -- account
 CREATE TABLE account (
     id serial primary key,
-    email varchar(64) unique,
-    password varchar(64)
+    email varchar(64) NOT NULL UNIQUE,
+    name varchar(16) NOT NULL,
+    hashed_password varchar(64) NOT NULL,
+    created_at timestamp default now() NOT NULL
 );
 
 -- scenario
 CREATE TABLE scenario (
     id serial primary key,
-    owner integer NOT NULL,
+    owner_id integer NOT NULL,
     title varchar(32) NOT NULL,
     body text NOT NULL,
-    updated_at timestamp default now(),
-    created_at timestamp default now(),
-    foreign key(owner) references account(id)
+    updated_at timestamp default now() NOT NULL,
+    created_at timestamp default now() NOT NULL,
+    foreign key(owner_id) references account(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );
+
+-- tag
+CREATE TABLE tag (
+    id serial primary key,
+    name varchar(16) NOT NULL UNIQUE
+);
+
+-- relation table
+-- tagging
+CREATE TABLE tagging (
+    scenario_id integer NOT NULL,
+    tag_id integer NOT NULL,
+    PRIMARY KEY(scenario_id, tag_id)
+);
+
 
